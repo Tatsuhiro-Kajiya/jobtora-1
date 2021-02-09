68879d71765d9a7ae1656691f3152773b5e9e08c
Tatsuhiro-Kajiya <75005529+Tatsuhiro-Kajiya@users.noreply.github.com>
2021-02-09 20:59:24 +0900

    編集機能の設定

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,6 @@
 class SpotsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_spot, only: [:show, :edit]
   
   def index
     @spots = Spot.order("created_at DESC")
@@ -18,12 +20,29 @@ class SpotsController < ApplicationController
   end
 
   def show
-    @spot = Spot.find(params[:id])
   end
 
-  
+  def edit
+    unless @spot.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    spot = Spot.find(params[:id])
+    if spot.update(spot_params)
+      redirect_to spot_path
+    else
+      render :edit
+    end
+  end
+
   private
   def spot_params
     params.require(:spot).permit(:name, :genre_id, :place_id, :explanation, :plan, :wifi_id, :budget, :image).merge(user_id: current_user.id)
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "spots#index"
-  resources :spots, only: [:new, :create, :index, :show]
+  resources :spots, only: [:new, :create, :index, :show, :edit, :update]
 end
